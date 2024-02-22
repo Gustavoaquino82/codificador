abdf50098fbdd20f050321dde5584880b453eeac
Gustavo Aquino <aquinogustavo82@gmail.com>
2024-02-22 00:07:03 -0300

    atualização

--- script.js
+++ script.js
@@ -1,100 +1,100 @@
-var textInput = document.querySelector(".input-texto");
-var outInput = document.querySelector("#box-resultado");
+let matrixCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+const input_texto = document.querySelector(".input-texto");
+const input_mensagem = document.querySelector(".mensagem");
+const btnCopiar = document.querySelector(".btn-copiar")
 
-function clearInputText() {
-  document.querySelector('.input-texto').value = "";
+
+
+
+//função para limpar texto do text area ao clicar no botão home
+function limparinputtexto () {
+    document.querySelector('.input-texto').value = "";
 }
-
-document.getElementById("div-aparece").style.display= 'none';
+//captura o id e esconde a div-aparece da tela
+document.getElementById("div-aparece").style.display = 'none';
 inputverificar();
 
-
-document.getElementsByClassName('btn-codificar').onclick = (e) => {
-  e.preventDefault();
-  const textoEncriptado = encriptar(input.value.toLowerCase());
-  mensagem.value = textoEncriptado;
-  input.value = "";
-  aparece()
-}
-
-
-document.getElementsByClassName('btn-decodificar').onclick = (e) => {
-  e.preventDefault();
-  const textoDecriptado = decodificar(input.value);
-  mensagem.value = textoDecriptado;
-  input.value = "";
-  aparece()
-}
-
-
-document.getElementsByClassName('btn-copiar').onclick = (e) => {
-  e.preventDefault();
-  const mensagem = document.querySelector("#box-resultado");
-  mensagem.select();
-  navigator.clipboard.writeText(mensagem.value)
-  mensagem.value = "";
-}
-
-function btnCodificar() {
-    const textoCodificado = encriptar(textInput.value)
-    outInput.value =textoCodificado
-    outInput.style.backgroundImage="none"
+// caputura o id no momento do click e direciona para o metódo que codifica o texto
+document.getElementsByClassName('btn-codificar').onclick = (e) =>{
+    e.preventDefault();
+    const textoCodificado = criptografar(input_texto.value.toLowercase())
+    input_mensagem.value = textoCodificado;
+    input_texto.value = "";
     aparece()
 }
-function encriptar(stringCodificada) {
-    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringCodificada =stringCodificada.toLowerCase();
 
-    for (let i=0; i<matrizCodigo.length; i++){
-        if( stringCodificada.includes(matrizCodigo[i][0])) {
-            stringCodificada =stringCodificada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+// caputura o id no momento do click e direciona para o metódo que desencripta o texto
+document.getElementsByClassName('btn-decodificar').onclick = (e) => {
+    e.preventDefault();
+    const textoDecodificado = descriptografar(input_texto.value);
+    input_mensagem.value = textoDecodificado;
+    input_texto.value = "";
+}
+
+// caputura o id no momento do click e faz a validação de copiar o texto
+document.getElementsByClassName('btn-copiar').onclick = (e) => {
+    e.preventDefault();
+    input_mensagem.select();
+    navigator.clipboard.writeText(input_mensagem.value)
+    input_mensagem.value = "";
+}
+
+//função que faz o botão de codificar funcionar
+function btnCodificar(){
+const textoCodificado = criptografar(input_texto.value)
+    input_mensagem.value = textoCodificado
+    input_mensagem.style.backgroundImagem = "none"
+    aparece()
+}
+// função de criptografia
+function criptografar(stringCodificada){
+    for (let i = 0; i < matrixCodigo.length; i++) {
+        if (stringCodificada.includes(matrixCodigo [i][0])){
+        stringCodificada = stringCodificada.replaceAll(matrixCodigo[i][0], matrixCodigo[i][1])
         }
     }
     return stringCodificada;
 }
-
+//função que faz o btão de decodificar funcionar
 function btnDecodificar(){
-    const textoCodificado = desencriptar(textInput.value)
-    outInput.value = textoCodificado
-    outInput.style.backgroundImage="none"
+    const textoDecodificado = descriptografar(input_texto.value)
+    input_mensagem.value = textoDecodificado
+    input_mensagem.style.backgroundImagem = "none"
     aparece()
 }
-function desencriptar (stringDecodificada) {
-    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
-    stringDecodificada =stringDecodificada.toLowerCase();
-
-    for (let i=0; i<matrizCodigo.length; i++){
-        if (stringDecodificada.includes(matrizCodigo[i][1])){
-            stringDecodificada = stringDecodificada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+// função de decriptografar
+function descriptografar(stringDecodificada){
+    for( let i = 0; i < matrixCodigo.length; i++){
+        if (stringDecodificada.includes(matrixCodigo[i][1])){
+            stringDecodificada = stringDecodificada.replaceAll(matrixCodigo[i][1], matrixCodigo[i][0])
         }
     }
     return stringDecodificada;
 }
-
-function copiar() {
-    const msg = document.querySelector(".mensagem");
+// função do botão de copiar
+function copiar (){
+    const msg = document.querySelector(".mensagem")
     msg.select();
-    navigator.clipboard.writeText(msg.value)    
-    alert("Texto copiado para a área de transferência!");
+    navigator.clipboard.writeText(msg.value)
 }
-function aparece() {
+//manipula o DOM para que a imagem e o texto suma ao clicar nos botões de codificar e decodificar
+function aparece(){
     document.getElementById("div-desaparece").style.display = 'none';
-    document.getElementById("div-aparece").style.display = 'block';
-      }
-  
-  //manipula o dom para que alguns componentes apareçam e desapareçam da tela
-  function home() {
+    document.getElementById("div-aparece").style.display = 'block' ;
+}
+// função de reinicar a pagina e limpar o texto do input texto
+function home(){
     document.getElementById("div-aparece").style.display = 'none';
     document.getElementById("div-desaparece").style.display = 'block';
-    return clearInputText();
-  }
-    function inputverificar() {
+    return limparinputtexto();
+}
+
+function inputverificar(){
     var inputPalavra = document.querySelector(".input-texto");
     inputPalavra.addEventListener("keypress", function (e) {
-      var keyCode = (e.keyCode ? e.keyCode : e.which);
-
-      if (keyCode > 47 && keyCode < 65) {
-        e.preventDefault();
-    }
-});
-}  
+        var keyCode = (e.keyCode ? e.keyCode : e.wich);
+        if(keyCode > 32 && keyCode < 65){
+            e.preventDefault();
+        }
+    })
+}
